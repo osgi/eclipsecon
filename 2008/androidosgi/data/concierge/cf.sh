48dc2d04d589244337ad406bf6b4b5cd3c11f4a6
BJ Hargrave <bjhargrave@users.noreply.github.com>
2008-03-09 20:38:24 +0000

    initial drop

---
+++ 2008/androidosgi/data/concierge/cf.sh
@@ -0,0 +1,1 @@
+/system/bin/dalvikvm -Xbootclasspath:/system/framework/core.jar -Xverify:none -Dch.ethz.iks.concierge.debug.classloading=true -classpath /data/concierge/ch.ethz.iks.concierge.framework_1.0.2.RC2.jar ch.ethz.iks.concierge.framework.Framework
