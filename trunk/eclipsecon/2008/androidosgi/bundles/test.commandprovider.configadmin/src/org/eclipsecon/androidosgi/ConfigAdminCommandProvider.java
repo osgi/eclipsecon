08520010fa1085d8150f2e862e8a91f9ad0ea184
hargrave@us.ibm.com <hargrave@us.ibm.com@141002c2-272c-0410-86ac-80daed660731>
2008-03-18 16:14:10 +0000

    remove unused import

--- trunk/eclipsecon/2008/androidosgi/bundles/test.commandprovider.configadmin/src/org/eclipsecon/androidosgi/ConfigAdminCommandProvider.java
+++ trunk/eclipsecon/2008/androidosgi/bundles/test.commandprovider.configadmin/src/org/eclipsecon/androidosgi/ConfigAdminCommandProvider.java
@@ -2,7 +2,6 @@ package org.eclipsecon.androidosgi;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Dictionary;
 import java.util.Enumeration;
